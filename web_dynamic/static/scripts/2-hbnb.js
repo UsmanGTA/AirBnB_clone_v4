cfbc9e98da7beb0ec1b89c35968f80c98c440299
UsmanGTA <usmangta@gmail.com>
2020-09-27 00:06:20 +0000

    added initial files for task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,35 @@
+// Check if the HTML/CSS has been loaded
+$(document).ready(() => {
+
+  // Check the API status and return status
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', json => {
+    if (json['status'] === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  // Check if the input checkbox has been checked
+  $('input').click(() => {
+    const ids = {}; // Reset the list of checked boxes with every click
+    const temp = $('input:checked');
+    let checkedBoxes = $('input:checked');
+
+    for (let index = 0; index < temp.length; index++) {
+      ids[checkedBoxes.attr('data-id')] = checkedBoxes.attr('data-name'); // Append {id : name}
+      checkedBoxes = checkedBoxes.slice(1); // Remove the first element in the list since attr() pulls the first occurrence
+    }
+
+    // Update and add selected boxes into the search box
+    // If ids{} has stuff in it, print it on the main, else reset if all the boxes are unchecked
+    const amenities = Object.values(ids);
+    if (amenities.length !== 0) {
+      $('.amenities h4').text(amenities.join(', '));
+    } else {
+      $('.amenities h4').text('');
+    }
+  });
+
+
+});
